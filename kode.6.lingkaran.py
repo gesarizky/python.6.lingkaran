a791b1a34805da5ff8fe28474305eb29b34e5b07
Gesa Rizky Nugraha <109656432+gesarizky@users.noreply.github.com>
2022-08-15 15:02:12 +0700

    Add files via upload

---
+++ kode.6.lingkaran.py
@@ -0,0 +1,10 @@
+# Menginput jari-jari Lingkaran
+jari = float(input('Tulis Jari-jari Lingkaran: '))
+
+# Hitung Luas dan Keliling Lingkaran
+luas = 3.14 * jari * jari
+keliling = 3.14 * 2 * jari
+
+#Menampilkan Hasil Perhitungan
+print('Luas Lingkaran adalah %0.2f' %luas)
+print('keliling Lingkaran adalah %0.2f' %keliling)
